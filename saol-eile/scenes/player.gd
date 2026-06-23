61814cfa4f2d858ec84ddb869de2693ac11f8741
cyle <clabhta@gmail.com>
2026-06-22 21:28:30 -0500

    basic scene, wasd to test, implement tilegrid, a* tmr

---
+++ saol-eile/scenes/player.gd
@@ -0,0 +1,34 @@
+extends CharacterBody3D
+
+@export var speed := 5.0
+@export var gravity := 20.0
+
+func _physics_process(delta: float) -> void:
+	var input_dir := Vector3.ZERO
+	
+	if Input.is_key_pressed(KEY_W):
+		input_dir.z -= 1
+		print('w')
+	if Input.is_key_pressed(KEY_A):
+		input_dir.x -= 1
+		print('a')
+	if Input.is_key_pressed(KEY_S):
+		input_dir.z += 1
+		print('s')
+	if Input.is_key_pressed(KEY_D):
+		input_dir.x += 1
+		print('d')
+	
+	input_dir = input_dir.normalized()
+	
+	velocity.x = input_dir.x * speed
+	velocity.z = input_dir.z * speed
+	
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+	else:
+		velocity.y = 0
+
+#
+
+	move_and_slide()
